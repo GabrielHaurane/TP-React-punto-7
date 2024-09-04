7632e0e0a4885ae60b24d99fb3ae9737a06fd471
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-09-03 22:40:57 -0300

    maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-
-import './App.css'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./App.css";
+import EmpleadoList from './components/EmpleadoList'
 function App() {
- 
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     
+      <div className="container">
+        <EmpleadoList></EmpleadoList>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/EmpleadoAvatar.jsx
@@ -0,0 +1,13 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const EmpleadoAvatar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EmpleadoAvatar;
