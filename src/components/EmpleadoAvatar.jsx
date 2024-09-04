891d69b7a1a1ce8fd2a698b79bbde60d90f2d806
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-09-03 23:32:51 -0300

    punto 7 terminado

--- src/components/EmpleadoAvatar.jsx
+++ src/components/EmpleadoAvatar.jsx
@@ -1,11 +1,11 @@
-
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const EmpleadoAvatar = () => {
+const EmpleadoAvatar = ({pic, fullName}) => {
     return (
         <div>
-            
+            <img className='avatar' src={pic} alt= {`foto de ${fullName}`} />
         </div>
     );
 };
--- src/components/EmpleadoRow.jsx
+++ src/components/EmpleadoRow.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-
-const EmpleadoRow = () => {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Badge } from 'react-bootstrap';
+import EmpleadoAvatar from './EmpleadoAvatar';
+const EmpleadoRow = ({empleado}) => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <Card className='w-75 mb-2'>
+            <Card.Body>
+                <div className='container'>
+                    <div className='w-100 row'>
+                        <div className='w-25'>
+                            <EmpleadoAvatar
+                            key={empleado.fullName}
+                            fullName={empleado.fullName}
+                            pic={empleado.pic}
+                            ></EmpleadoAvatar>
+                        </div>
+                    <div className='w-75'>
+                        
+                            <h3>{empleado.fullName}</h3>
+                            <span>{empleado.title}</span>
+                            <Badge bg="info" className='ms-3'>{empleado.department}</Badge>
+                        
+                    </div>
+                    </div>
+                </div>
+            </Card.Body>
+        </Card>
+        </>
     );
 };
 
